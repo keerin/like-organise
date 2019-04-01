acc2002637850c4339c1a758a444b4e2608a31bf
Kieran McNairn <thekieran@gmail.com>
2019-04-01 17:23:06 +0000

    Omniauth-twitter added and configured. Not working. env variables?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190401114019) do
+ActiveRecord::Schema.define(version: 20190401164054) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20190401114019) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

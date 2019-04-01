2a694b3f7bbb0398d11eb864a3b0798c49eb2b9c
Kieran McNairn <thekieran@gmail.com>
2019-04-01 19:07:09 +0000

    Amends devise.rb to use correct ENV variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,6 +297,6 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   
-  config.omniauth :twitter, "ENV[TWITTER_KEY]", "ENV[TWITTER_SECRET]" 
+  config.omniauth :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"] 
   
 end
